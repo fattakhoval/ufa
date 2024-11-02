c308ccecb844be5b69a9ed9e6ac6b972387a7ed3
toejamow <80569639+toejamow@users.noreply.github.com>
2024-11-02 10:34:19 +0500

    окончание верстки

--- laravel/resources/views/event.blade.php
+++ laravel/resources/views/event.blade.php
@@ -4,7 +4,7 @@
 
 <h2>Предложите мероприятие</h2>
 
-<form action="{{route('applications.store')}}" method="post" class="d-flex flex-column align-items-center">
+<form action="{{route('applications.store')}}" method="post" class="d-flex flex-column align-items-center form-up" style="margin: 0 0 2em 0;">
    @csrf
 
     <input type="hidden" name="id" value="1">
@@ -13,7 +13,7 @@
     <input name="description" type="text" placeholder="Описание" required>
     <input name="days" type="text" placeholder="Дата проведения" required>
     <input type="hidden" name="status" value="Новая">
-    <button type="submit">Подать заявку</button>
+    <input type="submit" value="Подать заявку">
 </form>
 
 @endsection
